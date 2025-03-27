1987f0d39f2235bc7ff6a5904edda5efca05c478
Saskia Friedrich <saskia.friedrich@iubh-fernstudium.de>
2025-03-27 12:17:00 +0100

    header script ausgelagert

--- loader.js
+++ loader.js
@@ -1,2 +1,3 @@
-$("header").load("components/header.html");
-$("footer").load("components/footer.html");
+$("header").load("components/header.html", function () {
+    $.getScript("components/header-item-active.js");
+});$("footer").load("components/footer.html");
